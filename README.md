7f0f211f3d13d5f1a82d3977c40691b56e2d6c9b
beef <beeflawg@gmail.com>
2018-11-14 13:55:31 -0700

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # eatdaburger
+
+## I have the devour button working correctly,  however I keep getting errors when trying add and remove burgers from the lists.
+### 1: Error 404, when trying to link the ajax js into the main.handlebars, says it can't locate the file
+### 2: Error 500, when I hardcode it into main.handlebars,
+#### TypeError: Cannot read property 'name' of undefined at /Users/befort/Desktop/uofa/Homework/eatdaburger/controllers/burger_controller.js:23:14
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -20,7 +20,7 @@ router.post("/api/burgers", function(req, res) {
   burger.insertOne([
     "burger_name"
   ], [
-    req.body.burger_name
+    req.body.name
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -33,9 +33,22 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.updateOne({
-    devoured: req.body.devoured
+    devoured: true
   }, condition, function(result) {
     if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
